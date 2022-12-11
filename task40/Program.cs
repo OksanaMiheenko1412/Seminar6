38b4f2c3c4ae366d576e28c66bca742dcfe2d278
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-11 11:35:17 +0700

    / add  commit task40

---
+++ task40/Program.cs
@@ -0,0 +1,16 @@
+﻿// task39 Напишите программу, которя на вход принримает 3 числа и проверяет,
+// может лис уществовать треугольник с сторонами такой длины. Теорема о 
+// неравенсте треугольника: каждая сторона треугольника меньше суммы двух
+// других сторон
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число: ");
+int b  = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число: ");
+int c  = Convert.ToInt32(Console.ReadLine()); 
+if (a < b + c && b < a + c && c < a + b)
+    Console.WriteLine("Может");
+else
+    Console.WriteLine("Не может");
