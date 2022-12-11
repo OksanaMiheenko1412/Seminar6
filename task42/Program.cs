78265b33314901f1a62bceba27baff8da28e92bc
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-11 11:47:11 +0700

    / add  commit task42

---
+++ task42/Program.cs
@@ -0,0 +1,13 @@
+﻿//task42 напишите программу,которая будет преобразовывать десятичное число
+//в двоичное
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+string result = String.Empty;// String.Empty - заведение пустой строки
+while (n > 0)
+{
+    result = Convert.ToString(n % 2) + result;
+    n /= 2;
+}
+Console.WriteLine(result);
